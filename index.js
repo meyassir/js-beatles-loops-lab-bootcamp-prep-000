82ae03d378c534c48434588740533dd9fd7f6971
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 20:30:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay(musician, instruments)
+{
+  let beatles = new array();
+  for(let i = 0; i<musician.length; i++) {
+    beatles[i] = `${musician[i]} plays ${instruments[i]}`;
+    return beatles;
+  }
+}
